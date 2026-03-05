ba0ab7a17853f17d58d115d9edff5295efc93f9a
Zach Thompson <zthomp2@uw.edu>
2026-03-05 07:15:57 +0000

    moved attribute->file functions to .py files

---
+++ projects/projectDusty/FileWriter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#Function that creates a new file and writes strings from an array into said file#
+
+def write_inp_file(contents):
+    last_item = contents[-1] 
+    file_name_val = last_item.split('=')[-1].strip().replace("'", "").replace('"', "")
+    
+    with open(f"{file_name_val}.inp", "w", encoding="utf-8") as f:
+        f.writelines('\n'.join(contents))
+        
+    print(contents)
